1db0fb12497c15ee1e72edb94f17331aa2ae6856
vicchat <67198218+vicchat@users.noreply.github.com>
2020-07-12 09:04:17 -0700

    Create hm.js

---
+++ hm.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Vicchat</title>
+        <style>
+            body {
+                font-family: sans-serif;
+                text-align: Center;
+            }
+            a {
+                color: red;
+                text-decoration: none;
+            }
+            a:hover {
+                color:black
+            }
+        </style> 
+        
+    <body>
+        <h1>Welcome!</h1>
+        <a href=vicchat.github.io/tmc>Too many colours </a>
+        |
+        <a href=vicchat.github.io/party> Party Invotation </a>
+        |
+        <a href=vicchat.github.io/smalltravel> Tiny travel place </a>
+        |
+        <a href=vicchat.github.io/boxermodel> Are Boxers Beautifful? </a>
+        |
+        <a href="vicchat.github.io/search"> Search the Web </a>
+        |
+        <a  targrt="_blank" href="mailto:?Subject=New Message&Body=New body"> Write an email </a>
+        |
+        <a href="vicchat.github.io/pie">How to eat pie</a>
+        |
+        <a href="vicchat.github.io">Error</a>
+        |
+        <a href="vicchat.github.io/box">Boxes</a>
+        |
+        <a href = "vicchat.github.io/winston">Draw a beard</a>
+        |
+        <a href= "https://vicchat.github.io/symbols">Type Symbols </a>
+        |
+        <a href= "https://vicchat.github.io/table"> Table </a>
+    </body>
+</html>
