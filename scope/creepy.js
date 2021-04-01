f2df5706df991d635a83a0f4fb4500760c530f27
vicchat <67198218+vicchat@users.noreply.github.com>
2021-04-01 16:44:35 -0700

    Create creepy.js

---
+++ scope/creepy.js
@@ -0,0 +1,9 @@
+area.style.background = "grey";
+area.style.outlineColor = "red";
+frameCheck(function() {
+	if (playerX > 899 && playerX < 901 && playerY > 599 && playerY < 601) {
+		you_died("You went into the death zone!");
+	}
+});
+logChatMessage("for", "Go to x=900 y=600 for fun!", "Random Guy", "rgb(255, 81, 81)");
+logChatMessage("for", "Tip: Press F3 to show coordinates.", "Random Guy", "rgb(255, 81, 81)");
