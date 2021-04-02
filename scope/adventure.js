652b708976cf1865d5c4dbdfb17eace38abe147f
vicchat <67198218+vicchat@users.noreply.github.com>
2021-04-01 17:49:22 -0700

    Update adventure.js

--- scope/adventure.js
+++ scope/adventure.js
@@ -1,2 +1,2 @@
 drawMode(false);
-logchatmessage("system", "Information: This works the same as usual except you cannot draw anything!");
+logChatMessage("system", "Information: This works the same as usual except you cannot draw anything!");
