42f041ccdaf3b5773b55c490b51d2cfa1f850ee3
vicchat <67198218+vicchat@users.noreply.github.com>
2021-04-01 17:39:51 -0700

    Create glitchy.js

---
+++ scope/glitchy.js
@@ -0,0 +1,7 @@
+logChatMessage("for", "Hello! Press <i>W</i> to start!", "Host", "lightgreen");
+frameCheck(function(){
+	if (playerY === 40) {
+		openMenu("<h1>CONGRATULATIONS!</h1>You did as you were told!<br><br><br><br><br>Hi.,<br><br><br><button onclick=\"resume();\">K</button> <button onclick=\"resume(); you_died('Do not be rude!');\">No!</button>");
+		playerY = 50;
+	}
+});
