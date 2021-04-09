c7ed264f50dfdfb131cba8d98bd0e041361c6d0c
vicchat <67198218+vicchat@users.noreply.github.com>
2021-04-08 18:07:30 -0700

    Create proto.js

---
+++ scope/proto.js
@@ -0,0 +1,3 @@
+enterMode = true;
+area.style.backgroundImage = "url(https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/background-of-green-and-healthy-grass-royalty-free-image-1586800097.jpg?crop=0.68819xw:1xh;center,top&resize=400:*)";
+area.style.outline = "ridge 50px brown";
