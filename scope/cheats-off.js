0402e8ad8dfcbca29c67a96178cc33f9c797e2d4
vicchat <67198218+vicchat@users.noreply.github.com>
2021-04-08 17:26:21 -0700

    Update cheats-off.js

--- scope/cheats-off.js
+++ scope/cheats-off.js
@@ -3,3 +3,4 @@ cheatmode = false;
 frameCheck(function(){
   invincible = false;
 });
+logChatMessage("system", "This works the same as usual but you <i>can't</i> use any commands. I'd dare you to try!");
