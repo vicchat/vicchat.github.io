3d9400905b5d2dd54a63f2fb23493bf197575303
vicchat <67198218+vicchat@users.noreply.github.com>
2021-04-09 18:04:59 -0700

    Create odd.js

---
+++ scope/odd.js
@@ -0,0 +1,25 @@
+enterMode = true;
+var menus231 = document.getElementsByClassName("menu");
+for (var i = 0; i < menus231.length; i++) {
+	menus231[i].style.background = "lime";
+	menus231[i].style.borderRadius = "0px";
+	menus231[i].style.borderColor = "teal";
+	menus231[i].style.width = "700px";
+	menus231[i].style.height = "500px";
+}
+chat.style.background = "darkblue";
+chatmessage.style.background = "darkslategrey";
+area.style.background = "brown";
+area.style.outlineColor = "purple";
+html.style.background = "orange";
+for (var i = 0; i < plus.length; i++) {
+	plus[i].style.background = "black";
+}
+bar.style.background = "lime";
+bar.style.borderRadius = "0px";
+bar.style.borderColor = "teal";
+puffyThingy.src = "https://www.pinclipart.com/picdir/big/116-1169351_blushing-happy-face-emoji-printable-printable-emojis-clipart.png";
+coord.style.background = "green";
+coord.style.color = "lime";
+youDied.style.background = "rgba(0, 0, 255, 0.5)";
+logChatMessage("for", "Isit weird?", "Guy who made this awsome thingy", "purple");
