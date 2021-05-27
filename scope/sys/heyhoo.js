652a9c33e49dd6660c623a469b9326d3652917c0
vicchat <67198218+vicchat@users.noreply.github.com>
2021-05-26 17:24:44 -0700

    Create heyhoo.js

---
+++ scope/sys/heyhoo.js
@@ -0,0 +1,8 @@
+window.setInterval(function() {
+	if (playerX !== 50 || playerY !== 50) {
+		window.alert("Heeeeeeeeey!");
+		window.alert("Hoooooooooo!");
+		playerX = 50;
+		playerY = 50;
+	}
+}, 10);
