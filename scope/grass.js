b706dccb2fd1b9938b58c1c9c0792b6cab265b05
vicchat <67198218+vicchat@users.noreply.github.com>
2021-10-28 18:41:03 -0700

    Update grass.js

--- scope/grass.js
+++ scope/grass.js
@@ -1,7 +1,7 @@
 enterMode = true;
 
 html.style.background = "rgb(0, 157, 5)";
-area.style.outlineColour = "rgb(0, 175, 5)";
+area.style.outlineColor = "rgb(0, 175, 5)";
 window.setInterval(function() {
 	invincible = true;
 }, 10);
